5d1021c27c519a99ffeb8a4fe1f16f8ef9af3f9f
ayjzcoder <102896985+ayjzcoder@users.noreply.github.com>
2022-06-21 15:21:06 +0300

    Add project files.

---
+++ LAB1/HigherLowerGame.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+String choice ="Y";
+
+while(choice == "Y"){
+Random random = new Random();
+int guess=0;
+int no_guess = 0;
+int num = random.Next(1,100);
+
+    while (guess != num)
+    {
+        Console.WriteLine("Enter your guess:");
+        guess = Convert.ToInt32(Console.ReadLine());
+
+        if (guess > num)
+            Console.WriteLine("Lower \n");
+
+        if (guess < num)
+            Console.WriteLine("Higher \n");
+        no_guess++;
+        if (guess == num)
+           Console.WriteLine("\nYou have guessed it correctly \nThe number is: " + num + "\nYou have guessed: " + no_guess + " times");
+    }
+    Console.WriteLine("Do you want to contiue (Y/N):");
+    choice = Console.ReadLine().ToUpper();
+   
+}
+
